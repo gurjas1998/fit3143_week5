e0473360778ca439b2ce23633c68a7772478a0db
tnngu64 <69128698+tnngu64@users.noreply.github.com>
2020-08-31 13:10:34 +1000

    Update q4.c

--- q4.c
+++ q4.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	int rank, size;
 	int i;
 	double d;
-	char buffer[100];
+	char buffer[100]; //used 100 as stated in lab instructions
 	int position = 0;
 
 	MPI_Init(&argc, &argv);
@@ -24,15 +24,17 @@ int main(int argc, char *argv[])
 	    {
 		    printf("\nEnter a round number (>=0) and a real number: ");
 		    fflush(stdout);
-		    scanf("%d%lf", &i, &d);
-		    MPI_Pack(&i, 1, MPI_INT, buffer, 100, &position, MPI_COMM_WORLD);
-		    MPI_Pack(&d, 1, MPI_DOUBLE, buffer, 100, &position, MPI_COMM_WORLD);
+		    scanf("%d%lf", &i, &d); //round number stored in i and real number stored in d for Processor 0's copy of the 'code'
+		    MPI_Pack(&i, 1, MPI_INT, buffer, 100, &position, MPI_COMM_WORLD); //round number packed into buffer starting from position 0 of buffer 
+										      //MPI_Pack should determine the next position and store it back into variable position
+		    MPI_Pack(&d, 1, MPI_DOUBLE, buffer, 100, &position, MPI_COMM_WORLD); //Uses newly determined position to pack the real number into the buffer 
 		    //MPI_Send(buffer, position, MPI_PACKED, 1, 100, MPI_COMM_WORLD);
 		    
 	    }
         MPI_Bcast(buffer, 100, MPI_CHAR, 0, MPI_COMM_WORLD); //BCast is just a wrapper for SEND and RECV
+	    						     //Broadcast is sent from Processor 0, which is the buffer array containing the data we want to send
 	   
-	if(rank > 0)
+	if(rank > 0) //For all processors rank > 0, receive the data and unpack it, store it in their own copies of variables i and d
 	    {
 		    //MPI_Recv(buffer, 100, MPI_PACKED, 0, 100, MPI_COMM_WORLD, &status);
 		    MPI_Unpack(buffer, 100, &position, &i, 1, MPI_INT, MPI_COMM_WORLD);
@@ -41,7 +43,7 @@ int main(int argc, char *argv[])
 		    fflush(stdout);
 	    }
 	    
-	}while(i>=0);
+	}while(i>=0); //Keep prompting user for input until they enter a negative round number
 
 	MPI_Finalize();
 	return 0;
